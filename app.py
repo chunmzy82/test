379872513fb0ce0bfcd57d22e3efd65c84282874
unknown <you@example.com>
2024-11-13 17:47:11 +0100

    update app

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title("Heading of my Application")
-sf.header("Heading ")
+st.header("Heading ")
 st.subheader("Subheader Text")
 st.text("Normal Text")
 
@@ -13,6 +13,6 @@ st.markdown("<h3>this is 3 text</h3", unsafe_allow_html=True)
 st.markdown("<h4>this is 4 text</h4", unsafe_allow_html=True)
 st.markdown("<h5>this is 5 text</h5", unsafe_allow_html=True)
 st.markdown("<h6>this is 6 text</h6", unsafe_allow_html=True)
-ls
+
 
 
